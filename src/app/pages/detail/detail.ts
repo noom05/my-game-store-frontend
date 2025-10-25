23e70c4a5db9830a25177af0a3ed8844d3250a8c
noom05 <znoom204@gmail.com>
2025-10-25 20:00:51 +0700

    19.59

--- src/app/pages/detail/detail.ts
+++ src/app/pages/detail/detail.ts
@@ -110,7 +110,7 @@ export class Detail implements OnInit, OnDestroy {
 
   private apiBaseUrl(): string {
     // return 'https://games-database-main.onrender.com';
-    return 'http://localhost:3000';
+    return 'https://games-database-main.onrender.com';
   }
 
   confirmDelete(): void {
